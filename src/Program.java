fd425594090187b2e3fa06231b9ab075cc5358fa
Jeferson Gomes <jefe.gomes@outlook.com>
2023-03-22 11:57:04 -0300

    corrigindo sintaxe

--- src/Program.java
+++ src/Program.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
         try (var leitor = new Scanner(System.in)) {
+
             System.out.println("Informe os meses: ");
-            int entrada = leitor.nextInt();
-            int mesada = 50;
-            int resultado = entrada * mesada;        
+            
+               int entrada = leitor.nextInt();
+               int mesada = 50;
+               int resultado = entrada * mesada;        
             
             
             System.out.println("Voce tera "+ resultado + " reais em " + entrada + " meses");
