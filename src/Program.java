4d120f406ac2318e006bddec654a43998d6de216
Jeferson Gomes <jefe.gomes@outlook.com>
2023-03-22 11:38:14 -0300

    corrigindo erro de digitação

--- src/Program.java
+++ src/Program.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
         try (var leitor = new Scanner(System.in)) {
-            System.out.printf("Informe os meses: ");
+            System.out.println("Informe os meses: ");
             int entrada = leitor.nextInt();
             int mesada = 50;
             int resultado = entrada * mesada;        
             
             
-            System.out.printf("Você terá "+ resultado + " Reais");
+            System.out.println("Voce tera "+ resultado + " reais");
         }
         
     }
